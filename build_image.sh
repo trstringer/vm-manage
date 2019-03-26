3bd30b1eaf099e7605ba392a215ee76e69eb7082
Thomas Stringer <github@trstringer.com>
2019-03-25 21:13:16 -0400

    add docker components

---
+++ build_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COMMIT=$(git log -n1 --pretty='%h')
+REPO="trstringer/vm-manage"
+
+source venv/bin/activate
+
+pip freeze | grep -v "pkg-resources" > requirements.txt
+
+docker build -t "$REPO:$COMMIT" -t "$REPO:latest" .
