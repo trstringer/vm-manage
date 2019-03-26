19f405f43ed45b457991bd292f54270252396f3b
Thomas Stringer <github@trstringer.com>
2019-03-26 07:44:47 -0400

    add base routes

--- app/app.py
+++ app/app.py
@@ -10,3 +10,19 @@ def default_route():
     """Default root route"""
 
     return jsonify(dict(application='vm-manage'))
+
+@app.route('/vm', methods=['POST'])
+def create_vm():
+    """Create a VM"""
+
+    return 'Creating a VM!'
+
+@app.route('/vm/')
+@app.route('/vm/<vm_id>')
+def get_vm(vm_id: int = None):
+    """List all VMs or a single one"""
+
+    if vm_id is None:
+        return 'Get all VMs!'
+
+    return f'Getting VM {vm_id}!'
